300fa31975502a7f57da7cc7cb09c961332bcc47
jonatascortes <joncortes.on@gmail.com>
2025-09-22 22:53:30 -0300

    feat(refine): refine VerificaVelha to dynamically check the winner.

--- velha.cpp
+++ velha.cpp
@@ -15,9 +15,30 @@
 
 int VerificaVelha( int velha[3][3] )
 {
-	if(velha[0][2] == 1 and velha[1][2] == 1 and velha[2][2] == 1){
-		return 1;
+	int center_column = 1;
+	for(int row=0; row<3; row++){
+
+		int left_square = velha[row][center_column-1];
+		int center_square = velha[row][center_column];
+		int right_square = velha[row][center_column+1];
+
+		if(center_square == left_square && center_square == right_square && center_square > 0){
+			return center_square;
+		}
 	}
+
+	int center_row = 1;
+	for(int column=0; column<3; column++){
+
+		int up_square = velha[center_row-1][column];
+		int center_square = velha[center_row][column];
+		int down_square = velha[center_row+1][column];
+
+		if(center_square == up_square && center_square == down_square && center_square > 0){
+			return center_square;
+		}
+	}
+
 	return 0;
 }
 
